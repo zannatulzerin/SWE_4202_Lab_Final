7d227f8a1eb8985cb310c1b8f85da4c36b7dabaa
Zannatul Ferdous Zerin <95172922+zannatulzerin@users.noreply.github.com>
2022-05-25 17:55:06 +0600

    Add files via upload

---
+++ SWE-4202-Lab-04/Lab 04/Research Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    internal class Research_Article: Book
+    {
+        public int DOI;
+        public int published_date;
+        public string type;
+        
+        public string getInfo()
+        {
+            string info = Convert.ToString(id) + "\t" + title + "\t" + author + "\t" + DOI + "\t" + Convert.ToString(published_date) + type;
+            return info;
+        }
+    }
+}
---
+++ SWE-4202-Lab-04/Lab 04/Study Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    internal class Study_Books: Book
+    {
+        public int ISBN;
+        public string genre;
+
+        public string getInfo()
+        {
+            string info = Convert.ToString(id) + "\t" + title + "\t" + author + "\t" + ISBN + "\t" + Convert.ToString(quantity);
+            return info;
+            
+    }
+}
