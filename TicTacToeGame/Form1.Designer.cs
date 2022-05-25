e38601f04eec571b512b938a96e3eb14632968fe
Zannatul Ferdous Zerin <95172922+zannatulzerin@users.noreply.github.com>
2022-05-25 17:52:52 +0600

    Add files via upload

---
+++ TicTacToeGame/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace TicTacToeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(215, 170);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 60);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(307, 170);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 60);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(121, 246);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 56);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(215, 246);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 56);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(307, 246);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 56);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 10);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(121, 236);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(270, 10);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(202, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 207);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(296, 95);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 207);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(307, 355);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(541, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Player1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Player2:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TicTacToeGame/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int term = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button1.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button1.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button2.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button2.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button3.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button3.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button6.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button6.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button5.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button5.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button4.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button4.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button9.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button9.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                 (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                 || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                 || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button8.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button8.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            term++;
+            if (term % 2 != 0)
+            {
+                button7.Text = "X";
+            }
+            else if (term % 2 == 0)
+            {
+                button7.Text = "O";
+            }
+
+            if ((button1.Text == button2.Text && button2.Text == button3.Text) || (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button7.Text == button8.Text && button8.Text == button9.Text) || (button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text == button5.Text && button5.Text == button8.Text) || (button3.Text == button6.Text && button6.Text == button9.Text)
+                || (button1.Text == button5.Text && button5.Text == button9.Text) || (button3.Text == button5.Text && button5.Text == button7.Text))
+            {
+                MessageBox.Show("Win!");
+            }
+            else
+            {
+                if (term == 9)
+                {
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Text = "1";
+            button2.Text = "2";
+            button3.Text = "3";
+            button4.Text = "4";
+            button5.Text = "5";
+            button6.Text = "6";
+            button7.Text = "7";
+            button8.Text = "8";
+            button9.Text = "9";
+        }
+    }
+}
+
